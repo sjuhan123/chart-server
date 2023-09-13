0b2de16b6faeb2f77ae00b304ef372f38dfa674e
sjuhan123 <sjuhan123@gmail.com>
2023-09-13 19:43:20 +0900

    add chart mock data

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -13,6 +15,7 @@ server.use(
   })
 );
 server.use(router);
+// Listen to port
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
